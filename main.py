3b3b166d46d82af2397f81021d825a74b58ac062
EfimovaLena <107196110+EfimovaLena@users.noreply.github.com>
2022-12-03 20:08:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     corutina.send(None)
     @settings_of_input_and_output
     def main():
+        corutina.send(None)
         input_value = input()
         while input_value != '0':
             print('Результат: ', one_step(input_value.strip()))
